0f4e10f2ccbafcb91f00385ca38e9aea09c2c7eb
Jonathan Keane <jkeane889@gmail.com>
2019-02-19 15:09:00 -0500

    Uploading exerises 10 through 12 (and drills) to the repo

---
+++ drill11.rb
@@ -0,0 +1,20 @@
+shampoo = 11.64
+conditioner = 12.08
+sunscreen = 15.89
+soap = 4.99
+towel = 3.49
+
+print "\nShampoo\nConditioner\nSunscreen\nSoap\nTowel"
+print "What product listed below are you looking to purchase: "
+
+item = gets.chomp
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "A smaller number is #{smaller}"
--- ex10.rb
+++ ex10.rb
@@ -1,8 +1,8 @@
-print "How old are you?"
-age = gets.chomp
-print "How tall are you?"
-height = gets.chomp
-print "How much do you weigh?"
-weight = gets.chomp
+print "How old are you? "
+age = $stdin.gets.chomp
+print "How tall are you? "
+height = $stdin.gets.chomp.to_f
+print "How much do you weigh? " 
+weight = $stdin.gets.chomp.to_f
 
 puts "So, you're #{age} old, #{height} tall and weigh #{weight} pounds."
