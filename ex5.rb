f0108328f991e5d062e55155e36386356bd82005
Jonathan Keane <jkeane889@gmail.com>
2019-02-19 13:35:13 -0500

    Adding all exercises to repo

---
+++ ex5.rb
@@ -0,0 +1,20 @@
+my_name = 'Jonathan B. Keane'
+my_age = 29
+my_height = 71
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+my_height_in_cent = (my_height * 2.54)
+my_weight_in_kilograms = (my_weight * 0.4535)
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "My height in centimeters is #{my_height_in_cent}."
+puts "My weight in kilograms is #{my_weight_in_kilograms}."
